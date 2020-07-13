81c5db47303053ad5196f3349a851a36e339da99
unknown <snehanimbare15@gmail.com>
2020-07-13 23:43:48 +0530

    Salary using case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,14 +7,16 @@ isPartTime=1
 isFullTime=2
 
 var=$(( RANDOM % 3 ))
-if [[ $var -eq $isFullTime ]]
-then
-empHrs=8
-elif [[ $var -eq $isPartTime ]]
-then
-empHrs=4
-else
-empHrs=0
-fi
-salary=$(($empHrs*$empRatePerHr))
-echo "Salary= "$salary
+case "$var" in
+
+$isFullTime)
+	empHrs=8
+	;;
+$isPartTime)
+	empHrs=4
+	;;
+*)
+	empHrs=0
+	;;
+esac
+echo salary=$(($empHrs*$empRatePerHr))
