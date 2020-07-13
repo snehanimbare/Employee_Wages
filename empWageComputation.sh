deb308e2fead61b51190197210a9bee8c603e9fb
unknown <snehanimbare15@gmail.com>
2020-07-13 22:55:00 +0530

    Employee is Present or Absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,10 @@
 echo "Welcome to Employee Wage Computation Program"
+
+#!/bin/bash -x
+var=$(( RANDOM % 2 ))
+if [[ $var -eq 1 ]]
+then
+echo "Employee is present"
+else
+echo "Employee is absent"
+fi
