41d4b2700443d04fc78df0913ff49fa09686641b
unknown <snehanimbare15@gmail.com>
2020-07-13 22:59:59 +0530

    Salary

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,13 @@
 echo "Welcome to Employee Wage Computation Program"
 
 #!/bin/bash -x
+
+empRatePerHr=20
+empHrs=8
 var=$(( RANDOM % 2 ))
 if [[ $var -eq 1 ]]
 then
-echo "Employee is present"
+	echo salary=$(($empRatePerHr*$empHrs))
 else
-echo "Employee is absent"
+	echo salary=0
 fi
